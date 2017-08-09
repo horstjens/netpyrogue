990c4c2e0b40114967c6dc4f34c381bdd6253565
gatgeagent <gatgeagent@gmail.com>
2017-08-09 17:00:18 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='netpyrogue',
+        version='0.0.1',
+        packages=['', 'lib', 'lib.PodSixNet_Library'],
+        url='https://github.com/horstjens/netpyrogue',
+        license='GPL',
+        author='horstjens,gatgeagent',
+        author_email='',
+        description='NetPyRouge is a rouge-like multiplayer-based game.'
+)
